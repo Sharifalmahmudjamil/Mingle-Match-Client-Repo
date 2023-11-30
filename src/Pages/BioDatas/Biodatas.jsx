6aa79d7daaafa2a92988465c0f585c25dc7d6941
sharifalmahmudjamil <smjamil111@gmail.com>
2023-11-29 17:59:41 -1200

    implement a search funcationality in this website

--- src/Pages/BioDatas/Biodatas.jsx
+++ src/Pages/BioDatas/Biodatas.jsx
@@ -17,6 +17,7 @@ import './Biodatas.css'
 const Biodatas = () => {
   const [itemsPerPage,setItemsPerPage]=useState(6);
   const [currentPage,setCurrentPage]= useState(0);
+  const [search,setSearch] = useState('');
   const {count}=useLoaderData();
   console.log(count);
   
@@ -85,7 +86,7 @@ const handleNextPage =() =>{
           <Navbar></Navbar>
           <div className="flex mt-5">
              {/* left side Nav */}
-               <div className="w-52 h-72 rounded-xl bg-pink-400">
+               <div className="w-52 h-72 rounded-xl bg-pink-400 ">
                     <h1 className="text-xl text-center">Filter Option</h1>
                     <div className="mt-8">
                     <label className="ml-3">
@@ -97,14 +98,18 @@ const handleNextPage =() =>{
         </select>
       </label>
                     </div>
-                  
+                    <div className=" mt-5">
+            <input onChange={(e)=>setSearch(e.target.value)} type="text" placeholder="Search By Division" className="input input-bordered input-error w-full" />
+            </div>
       
                </div>
                {/* right side nav */}
                <div className="flex-1">
                <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
            {
-            filteredBioData.map(data=>
+            filteredBioData.filter((BioData)=>{
+              return search.toLowerCase() === ''?BioData:BioData.PresentDivision.toLowerCase().includes(search)
+            }).map(data=>
             <div key={data._id} className="card w-96 bg-gray-400 shadow-xl ml-8  border-2 border-pink-400">
               <h1 className="text-2xl text-center font-medium text-pink-500 mt-2"> BioData Information</h1>
             <figure className="px-10 pt-10">
